c6cafc8b62a99dd7db95ffeca8f047e8aef45c54
reynoldvinson <renvin_5@yahoo.co.id>
2019-11-29 14:36:41 +0700

    new BT

---
+++ cobaBT.py
@@ -0,0 +1,13 @@
+from bluetooth import *
+import time
+bd_addr = "00:21:13:03:80:B4"
+port = 1
+sock = BluetoothSocket (RFCOMM)
+sock.connect((bd_addr,port))
+print ('waiting')
+while 1:
+    data = sock.recv(1024)
+    data = string(data)
+    print (data)    
+    time.sleep(1)
+sock.close()
