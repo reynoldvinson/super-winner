f4581d85294260a4d990260dc992f5bfa7b62fd2
reynoldvinson <renvin_5@yahoo.co.id>
2019-12-05 16:19:48 +0700

    update cobaBT

--- cobaBT.py
+++ cobaBT.py
@@ -4,7 +4,7 @@ bd_addr = "00:21:13:03:80:B4"
 port = 1
 sock = BluetoothSocket (RFCOMM)
 sock.connect((bd_addr,port))
-print ('waiting')
+#print ('waiting')
 while 1:
     data = sock.recv(1024)
     data = data.decode()
