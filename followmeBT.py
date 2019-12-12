491e668423690d99603cc14ea164e094503dff3a
reynoldvinson <renvin_5@yahoo.co.id>
2019-12-12 16:28:08 +0700

    update

--- followmeBT.py
+++ followmeBT.py
@@ -63,8 +63,6 @@ bd_addr = "00:21:13:03:80:B4"
 port = 1
 sock = BluetoothSocket (RFCOMM)
 sock.connect((bd_addr,port))
-#data = sock.recv(1024)
-#data = data.decode()
 time.sleep(1)
 while 1:
     if vehicle.mode.name != "GUIDED":
