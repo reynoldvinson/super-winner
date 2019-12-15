1cccf70cf46c43650bf1edc6f3a3e886222e08ca
reynoldvinson <renvin_5@yahoo.co.id>
2019-12-15 16:06:01 +0700

    update

--- droneGPS.py
+++ droneGPS.py
@@ -1,11 +1,28 @@
 from dronekit import connect, VehicleMode, Command
 from pymavlink import mavutil
+from bluetooth import *
 import time
 
 COM = '/dev/ttyACM0'
 vehicle = connect(COM, wait_ready=True)
+    
+bd_addr = "00:21:13:03:80:B4"
+port = 1
+sock = BluetoothSocket (RFCOMM)
+sock.connect((bd_addr,port))
+time.sleep(1)
 
-while (True):
+while 1:
+    print(Drone GPS : )
     print(vehicle.gps_0)
     print(vehicle.location.global_relative_frame)
-    time.sleep(0.5)
+    data = sock.recv(1024)
+    
+    latit = data[:9]
+    longi = data[10:]
+    latit = float(latit)
+    longi = float(longi)
+    print (User GPS : )
+    print (latit, longi)
+    time.sleep(1)
+sock.close()
