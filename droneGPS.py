5ddf86d446eaf3ee8188870f5ff5736d1e86b7d7
reynoldvinson <renvin_5@yahoo.co.id>
2019-12-15 16:27:32 +0700

    l

--- droneGPS.py
+++ droneGPS.py
@@ -13,7 +13,7 @@ sock.connect((bd_addr,port))
 time.sleep(1)
 
 while 1:
-    print(Drone GPS : )
+    print("Drone GPS : ")
     print(vehicle.gps_0)
     print(vehicle.location.global_relative_frame)
     data = sock.recv(1024)
@@ -22,7 +22,7 @@ while 1:
     longi = data[10:]
     latit = float(latit)
     longi = float(longi)
-    print (User GPS : )
+    print ("User GPS : ")
     print (latit, longi)
     time.sleep(1)
 sock.close()
