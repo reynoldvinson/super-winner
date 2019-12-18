ae73b7cdc7af25771eb7eef3f58615d54d84959b
reynoldvinson <renvin_5@yahoo.co.id>
2019-12-18 13:11:45 +0700

    dadsadas

--- rangetest.py
+++ rangetest.py
@@ -52,7 +52,7 @@ def arm_and_takeoff(aTargetAltitude):
         time.sleep(1)
 
 
-arm_and_takeoff(6)
+arm_and_takeoff(5)
 
 #print("Set default/target airspeed to 0.5")
 #vehicle.airspeed = 0.5
@@ -65,7 +65,7 @@ time.sleep(1)
 while 1:
     if vehicle.mode.name != "GUIDED":
         continue
-    altitude = 6
+    altitude = 5
     data = sock.recv(1024)
     latitude = data[:9]
     longitude = data[10:]
